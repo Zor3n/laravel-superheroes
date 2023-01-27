42da48946933aaafdcef5a6785aa179172b1d58a
Renacuajo <braurrego@protonmail.com>
2023-01-27 16:55:53 -0500

    heroes grid complete

--- resources/views/components/heroes-grid.blade.php
+++ resources/views/components/heroes-grid.blade.php
@@ -1,3 +1,9 @@
+<style>
+    figure>img {
+        object-fit: cover;
+    }
+</style>
+
 <section id="heroes-grid" class="hero is-black has-background-black-ter is-small ">
     <section class="container py-6">
         <div class="hero-body">
@@ -19,17 +25,18 @@
                     <div class="column is-4">
                         <div class="card is-shady">
                             <div class="card-image">
-                                <figure class="image is-4by3">
-                                    <img src="img/{{ $hero['img'] }}" alt="Placeholder image" class="modal-button"
-                                        data-target="modal-image2">
-                                </figure>
+                                <a href="{{ $hero['url_fan_page'] }}" target="_blank" rel="noopener noreferrer">
+                                    <figure class="image is-4by3">
+                                        <img src="img/{{ $hero['img'] }}" alt="Placeholder image">
+                                    </figure>
+                                </a>
                             </div>
                             <div class="card-content">
                                 <div class="content">
                                     <h4>{{ $hero['name'] }}</h4>
                                     <p>{{ $hero['desc'] }}</p>
-                                    <span class="button is-link modal-button"
-                                        data-target="modal-image2">{{ __('heroes-grid.card_button') }}</span>
+                                    <a class="button is-link" target="_blank"
+                                        href="{{ $hero['url_fan_page'] }}">{{ __('heroes-grid.card_button') }}</a>
                                 </div>
                             </div>
                         </div>
@@ -57,26 +64,31 @@
 
     <section class="container">
         <div class="columns is-multiline is-centered mb-0">
-            <div class="column is-4">
-                <div class="card is-shady">
-                    <div class="card-image">
-                        <figure class="image is-4by3">
-                            <img src="img/superman.jpg" alt="Placeholder image" class="modal-button"
-                                data-target="modal-image2">
-                        </figure>
-                    </div>
-                    <div class="card-content">
-                        <div class="content">
-                            <h4>Superman</h4>
-                            <p>Superman, also known as the Man of Steel. His abilities include incredible
-                                super-strength, super-speed, invulnerability, freezing breath, flight, and heat-vision.
-                            </p>
-                            <span class="button is-link modal-button"
-                                data-target="modal-image2">{{ __('heroes-grid.card_button') }}</span>
+            @if (count($marvel_heroes) > 0)
+                @foreach ($marvel_heroes as $hero)
+                    <div class="column is-4">
+                        <div class="card is-shady">
+                            <div class="card-image">
+                                <a href="{{ $hero['url_fan_page'] }}" target="_blank" rel="noopener noreferrer">
+                                    <figure class="image is-4by3">
+                                        <img src="img/{{ $hero['img'] }}" alt="Placeholder image">
+                                    </figure>
+                                </a>
+                            </div>
+                            <div class="card-content">
+                                <div class="content">
+                                    <h4>{{ $hero['name'] }}</h4>
+                                    <p>{{ $hero['desc'] }}</p>
+                                    <a class="button is-link" target="_blank"
+                                        href="{{ $hero['url_fan_page'] }}">{{ __('heroes-grid.card_button') }}</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
+                @endforeach
+            @else
+                No records found!
+            @endif
         </div>
     </section>
 </section>
